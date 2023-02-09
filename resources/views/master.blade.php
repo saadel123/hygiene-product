8255ccff069be4da51fea74146b12d7a17731f1c
saadel123 <saad.sabir.ss@gmail.com>
2023-02-09 11:32:28 +0100

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="UTF-8">
+    @hasSection('title')
+        <title>@yield('title')</title>
+    @endif
+    @hasSection('description')
+        <meta name="description" content="@yield('description')">
+    @endif
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('assets/img/site.webmanifest') }}">
+    <!-- end favicon -->
+
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/aos/aos.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}">
+
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}"> --}}
+    {{-- <link rel="stylesheet" href="{{ asset('assets/fonts/icomoon/style.css') }}"> --}}
+
+
+
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+
+    <!-- Variables CSS Files. Uncomment your preferred color scheme -->
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/variables-green.css') }}">
+    @if (!request()->is('login'))
+        <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    @endif --}}
+
+
+    @yield('stylesheet')
+</head>
+
+<body>
+    {{-- @if (request()->is('login'))
+        @yield('content')
+    @else
+        @include('site.layouts.header') --}}
+
+    @yield('content')
+    {{--
+        @include('site.layouts.footer')
+        <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
+                class="bi bi-arrow-up-short"></i></a>
+        <div id="preloader"></div>
+    @endif --}}
+
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    {{-- <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script> --}}
+
+    <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
+
+
+    <script src="{{ asset('assets/js/jquery-3.3.1.min.js') }}"></script>
+    {{-- <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script> --}}
+
+    {{-- <script src="{{ asset('assets/js/popper.min.js') }}"></script> --}}
+    {{-- <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script> --}}
+
+    <!-- Template Main JS File -->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
+    @yield('javascripts')
+
+</body>
+
+
+</html>
