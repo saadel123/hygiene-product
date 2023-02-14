53af2dceb7bd33cd91304881571c6a6829a515c6
saadel123 <saad.sabir.ss@gmail.com>
2023-02-14 17:58:04 +0100

    Home page updated

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -57,12 +57,10 @@
         @include('site.layouts.header') --}}
 
     @yield('content')
-    {{--
-        @include('site.layouts.footer')
-        <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
-                class="bi bi-arrow-up-short"></i></a>
-        <div id="preloader"></div>
-    @endif --}}
+
+    @include('layouts.footer')
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 
 
     <!-- Vendor JS Files -->
